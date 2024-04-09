c2d9847a0122094ba6c438686c4b8f6242d5c7b3
unknown <dipeshhaldar787@gmail.com>
2024-04-09 22:35:16 +0530

    Backend Completed

--- server/index.js
+++ server/index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 const EmployeeModel = require("./models/users");
+const PORT = process.env.PORT || 3000;
+const dbHOST = process.env.DBHOST;
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(
-  "mongodb+srv://Dipesh:DipeshHaldar@cluster0.ypmscdk.mongodb.net/users?retryWrites=true&w=majority&appName=Cluster0"
-);
+mongoose.connect(dbHOST);
 
 app.post("/user", (req, res, next) => {
   EmployeeModel.create(req.body)
